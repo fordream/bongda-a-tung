9cf809cab2973bfa6dd2f39b82b10b76880b5c91
vnp.mr.truong <vnp.mr.truong@gmail.com>
2014-10-07 09:31:35 +0700

    add tab activity

--- BongDa/src/com/app/bongda/SplashActivity.java
+++ BongDa/src/com/app/bongda/SplashActivity.java
@@ -1,12 +1,55 @@
 package com.app.bongda;
 
 import android.app.Activity;
+import android.app.TabActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TabHost;
+import android.widget.TabHost.OnTabChangeListener;
+import android.widget.TabHost.TabSpec;
 
-public class SplashActivity extends Activity {
+public class SplashActivity extends TabActivity implements OnTabChangeListener {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.tabexampleslide);
+		getTabHost().setOnTabChangedListener(this);
+
+		addTab(HomeActivity.class, "Home", "Home", 0);
+
+	}
+
+	@Override
+	public void onTabChanged(String tabId) {
+
+	}
+
+	public void addTab(Class<?> activity, String tabSpect, String indicator,
+			int type) {
+		TabHost tabHost = getTabHost();
+		TabSpec firstTabSpec = tabHost.newTabSpec(tabSpect);
+		Intent intent = new Intent(this, activity);
+		intent.putExtra("type", type);
+		firstTabSpec.setIndicator(indicator).setContent(intent);
+		tabHost.addTab(firstTabSpec);
+	}
+
+	public void addTab(Class<?> activity, String tabSpect, View indicator) {
+		TabHost tabHost = getTabHost();
+		TabSpec firstTabSpec = tabHost.newTabSpec(tabSpect);
+		firstTabSpec.setIndicator(indicator).setContent(
+				new Intent(this, activity));
+		tabHost.addTab(firstTabSpec);
+	}
+
+	protected Context getContext() {
+		return this;
+	}
+
+	protected Activity getActivity() {
+		return this;
 	}
 
 }
