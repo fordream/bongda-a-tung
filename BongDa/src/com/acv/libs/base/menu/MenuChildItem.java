5de4a5b3c12c700cd734ac0e8712fb54d652bf79
vnp.mr.truong <vnp.mr.truong@gmail.com>
2014-10-07 08:49:03 +0700

    add and delete resource

--- BongDa/src/com/acv/libs/base/menu/MenuChildItem.java
+++
@@ -1,9 +0,0 @@
-package com.acv.libs.base.menu;
-
-public class MenuChildItem extends Menu {
-
-	public MenuChildItem(String name, int resImgge, boolean isHeader) {
-		super(name, resImgge, isHeader);
-	}
-
-}
--- BongDa/src/com/org/social/twitter/TwDialogListener.java
+++
@@ -1,7 +0,0 @@
-package com.org.social.twitter;
-
-public interface TwDialogListener {
-	public void onComplete(String value);
-
-	public void onError(String value);
-}
