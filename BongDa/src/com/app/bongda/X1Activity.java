7b4f4f5d3cafca11a3837d85939f5e05866ef183
NamLH <user@USER-M36UP05GFY>
2014-10-10 07:49:18 +0700

    test api

--- BongDa/src/com/app/bongda/X1Activity.java
+++ BongDa/src/com/app/bongda/X1Activity.java
@@ -1,19 +1,28 @@
 package com.app.bongda;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 
 import com.app.bongda.base.BaseActivtiy;
+import com.app.bongda.callback.APICaller;
+import com.app.bongda.callback.APICaller.ICallbackAPI;
 import com.app.bongda.fragment.DuDoanKetQuaFragment;
 import com.app.bongda.fragment.LiveScoreFragment;
 import com.app.bongda.fragment.PhongDoDoiDauFragment;
 import com.app.bongda.fragment.TuongThuatTranLiveScoreFragment;
 import com.app.bongda.fragment.TyLeDuDoanFragment;
+import com.app.bongda.util.ByUtils;
+import com.app.bongda.util.CommonAndroid;
 
 public class X1Activity extends BaseActivtiy {
-
+	Context context;
+	ICallbackAPI callbackAPI;
 	// live score
 	// live cac tran -> xem truong thuat -> ty le du doan
 	//									 -> du doan ket qua
@@ -22,7 +31,35 @@ public class X1Activity extends BaseActivtiy {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		context = this;
+		callbackAPI = new ICallbackAPI() {
+			@Override
+			public void onSuccess(String response) {
+				String string_temp = CommonAndroid.parseXMLAction(response);
+				CommonAndroid.showDialog(context, "data:" + string_temp , null);
+				
+				try {
+					JSONObject jsonObject = new JSONObject(string_temp);
+					
+				} catch (JSONException e) {
+//					CommonAndroid.showDialog(context, e.getMessage() , null);
+				}
+				
+			}
+
+			@Override
+			public void onError(String message) {
+			}
+		};
+		
+		CallAPI();
 		showFragment(new LiveScoreFragment(liveScoreOnItemClickListener));
+	}
+	
+	public void CallAPI(){
+		new APICaller(this).callApi("", true,
+				callbackAPI, ByUtils.wsFootBall_Quocgia);
+		
 	}
 	
 	OnItemClickListener liveScoreOnItemClickListener = new OnItemClickListener() {
