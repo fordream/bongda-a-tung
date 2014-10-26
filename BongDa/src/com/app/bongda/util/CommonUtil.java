d35c8e5a081758afe4ae96ecb1e762b31d22feec
NamLH <user@USER-M36UP05GFY>
2014-10-27 00:51:54 +0700

    add

---
+++ BongDa/src/com/app/bongda/util/CommonUtil.java
@@ -0,0 +1,13 @@
+package com.app.bongda.util;
+
+import java.util.ArrayList;
+
+public class CommonUtil
+{
+    public static ArrayList listQuanTam = new ArrayList();
+
+    public CommonUtil()
+    {
+    }
+
+}
