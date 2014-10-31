662ce0f2310026b39632ee9d9841294c55681330
namlh <namlh@atmarkcafe.org>
2014-10-31 18:32:36 +0700

    game du doan

--- BongDa/src/com/app/bongda/fragment/DuDoanKetQuaFragment.java
+++ BongDa/src/com/app/bongda/fragment/DuDoanKetQuaFragment.java
@@ -15,15 +15,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.app.bongda.R;
 import com.app.bongda.base.BaseFragment;
 import com.app.bongda.base.BongDaBaseAdapter;
 import com.app.bongda.callback.APICaller;
 import com.app.bongda.callback.APICaller.ICallbackAPI;
+import com.app.bongda.model.DuDoanKetQua;
 import com.app.bongda.model.LiveScore;
 import com.app.bongda.util.ByUtils;
 import com.app.bongda.util.CommonAndroid;
+import com.app.bongda.util.CommonUtil;
 import com.app.bongda.view.HeaderView;
 
 public class DuDoanKetQuaFragment extends BaseFragment {
@@ -85,17 +88,68 @@ public class DuDoanKetQuaFragment extends BaseFragment {
 			public void onSuccess(String response) {
 				 CommonAndroid.showDialog(getActivity(), "data2:" +
 						 response , null);
-//				String string_temp = CommonAndroid.parseXMLAction(response);
-//				if (!string_temp.equalsIgnoreCase("")) {
-//					// CommonAndroid.showDialog(getActivity(), "data2:" +
-//					// string_temp , null);
-////					 Log.e("data",string_temp);
-//				
-//						countryAdapter.notifyDataSetChanged();
-//					} catch (JSONException e) {
-//					}
-//
-//				}
+				String string_temp = CommonAndroid.parseXMLAction(response);
+				if (!string_temp.equalsIgnoreCase("")) {
+					try {
+
+						ArrayList<JSONObject> array = new ArrayList<JSONObject>();
+						array.clear();
+						JSONArray jsonArray = new JSONArray(string_temp);
+						if(jsonArray.length() == 0){
+							Toast.makeText(getActivity(), getResources().getString(R.string.giaichuabatdau), Toast.LENGTH_LONG).show();
+						}
+						for (int i = 0; i < jsonArray.length(); i++) {
+							try {
+								array.add(jsonArray.getJSONObject(i));
+							} catch (JSONException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						}
+						Collections.emptyList();
+						Collections.sort(array, new Comparator<JSONObject>() {
+
+							@Override
+							public int compare(JSONObject lhs, JSONObject rhs) {
+								// TODO Auto-generated method stub
+
+								try {
+									return (lhs.getString("sTenGiai").toLowerCase().compareTo(rhs.getString("sTenGiai").toLowerCase()));
+								} catch (JSONException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+									return 0;
+								}
+							}
+						});
+
+						String sTenDoiNha;
+						String sTenDoiKhach;
+						String iCN_BanThang_DoiNha;
+						String iCN_BanThang_DoiKhach;
+						String iC0; //ngay
+						String sThoiGian;
+						String sTyLe_ChauAu; //"2.20*3.10*2.35"
+						String sTyLe_ChapBong ; //":"0.87*0 : 1\/4*0.83"
+						String sTyLe_TaiSuu ;//":"0.98*2 1\/4*0.72"
+						for (int i = 0; i < array.size(); i++) {
+							sTenDoiNha = array.get(i).getString("sTenDoiNha");
+							sTenDoiKhach = array.get(i).getString("sTenDoiKhach");
+							iCN_BanThang_DoiNha = array.get(i).getString("iCN_BanThang_DoiNha");
+							iCN_BanThang_DoiKhach = array.get(i).getString("iCN_BanThang_DoiKhach");
+							iC0 = array.get(i).getString("iC0");
+							sThoiGian = array.get(i).getString("sThoiGian");
+							sTyLe_ChauAu =array.get(i).getString("sTyLe_ChauAu");
+							sTyLe_ChapBong = array.get(i).getString("sTyLe_ChapBong");
+							sTyLe_TaiSuu = array.get(i).getString("sTyLe_TaiSuu");
+							countryAdapter.addItem(new DuDoanKetQua(false, sTenDoiNha, sTenDoiKhach ,iCN_BanThang_DoiNha,iCN_BanThang_DoiKhach, iC0,sThoiGian,sTyLe_ChauAu ,sTyLe_ChapBong, sTyLe_TaiSuu));
+							Log.e("data", "" + array.get(i));
+
+						}
+						countryAdapter.notifyDataSetChanged();
+					} catch (JSONException e) {
+					}
+				}
 
 			}
 
