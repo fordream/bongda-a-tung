67935616f5848492c249f988105351062a527412
Vương Văn Trường <truongvv@atmarkcafe.org>
2014-11-04 08:15:55 +0700

    add

--- BongDa/src/com/app/bongda/MainSplashActivity.java
+++ BongDa/src/com/app/bongda/MainSplashActivity.java
@@ -1,5 +1,6 @@
 package com.app.bongda;
 
+import android.app.Activity;
 import android.app.AlertDialog.Builder;
 import android.app.Dialog;
 import android.content.DialogInterface;
@@ -10,6 +11,7 @@ import android.os.Handler;
 import android.os.Message;
 import android.view.View;
 import android.view.animation.RotateAnimation;
+import android.widget.Toast;
 
 import com.app.bongda.base.BaseActivtiy;
 import com.app.bongda.callback.APICaller.ICallbackAPI;
@@ -18,26 +20,26 @@ import com.app.bongda.service.BongDaServiceManager.BongDaServiceManagerListener;
 import com.app.bongda.util.ByUtils;
 import com.app.bongda.util.CommonAndroid;
 
-public class MainSplashActivity extends BaseActivtiy {
+public class MainSplashActivity extends Activity {
 	private View ic_logo;
 	private Handler handler = new Handler() {
 		@Override
 		public void dispatchMessage(Message msg) {
 			super.dispatchMessage(msg);
 
-			if (!isFinishing()) {
-				int start = rotate;
-				rotate = rotate + 10;
-				float pivotX = ic_logo.getWidth() / 2;
-				float pivotY = ic_logo.getHeight() / 2;
-				RotateAnimation animation = new RotateAnimation(start, rotate,
-						pivotX, pivotY);
-				animation.setDuration(10);
+			// if (!isFinishing()) {
+			int start = rotate;
+			rotate = rotate + 10;
+			float pivotX = ic_logo.getWidth() / 2;
+			float pivotY = ic_logo.getHeight() / 2;
+			RotateAnimation animation = new RotateAnimation(start, rotate,
+					pivotX, pivotY);
+			animation.setDuration(10);
 
-				ic_logo.startAnimation(animation);
+			ic_logo.startAnimation(animation);
 
-				handler.sendEmptyMessageDelayed(0, 11);
-			}
+			handler.sendEmptyMessageDelayed(0, 11);
+			// }
 		}
 
 		int rotate = 0;
@@ -46,36 +48,50 @@ public class MainSplashActivity extends BaseActivtiy {
 	@Override
 	protected void onCreate(Bundle arg0) {
 		super.onCreate(arg0);
-		overridePendingTransition(R.anim.bot_to_top, R.anim.nothing);
+		// overridePendingTransition(R.anim.bot_to_top, R.anim.nothing);
 		setContentView(R.layout.mainsplash);
 		ic_logo = (View) findViewById(R.id.ic_logo);
 
-		handler.sendEmptyMessage(0);
-		BongDaServiceManager.getInstance().onResume(
-				new BongDaServiceManagerListener() {
+		// handler.sendEmptyMessage(0);
+		ic_logo.post(new Runnable() {
 
-					@Override
-					public void onSuccess() {
-						onCheckForNetwork();
-					}
-
-					@Override
-					public void onFail() {
-
-					}
-
-					@Override
-					public void onDisconnected() {
-
-					}
-				});
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				onCheckForNetwork();
+			}
+		});
 
 	}
 
+	@Override
+	protected void onResume() {
+		super.onResume();
+		// onCheckForNetwork();
+		// BongDaServiceManager.getInstance().onResume(
+		// new BongDaServiceManagerListener() {
+		//
+		// @Override
+		// public void onSuccess() {
+		//
+		// }
+		//
+		// @Override
+		// public void onFail() {
+		// }
+		//
+		// @Override
+		// public void onDisconnected() {
+		// }
+		// });
+	}
+
 	private void onCheckForNetwork() {
-		if (!isFinishing()) {
+		if (!MainSplashActivity.this.isFinishing()) {
+			CommonAndroid.toast(this, "finish");
 			return;
 		}
+
 		if (!CommonAndroid.NETWORK.haveConnectTed(this)) {
 			Builder builder = new Builder(this);
 			builder.setMessage(R.string.check_network);
@@ -91,7 +107,8 @@ public class MainSplashActivity extends BaseActivtiy {
 				@Override
 				public void onClick(DialogInterface dialog, int which) {
 					finish();
-					overridePendingTransition(R.anim.bot_to_top, R.anim.nothing);
+					// overridePendingTransition(R.anim.bot_to_top,
+					// R.anim.nothing);
 				}
 			});
 
@@ -117,8 +134,9 @@ public class MainSplashActivity extends BaseActivtiy {
 						intent.putExtra("socre", string_temp);
 
 						startActivity(intent);
-						finish();
-						overridePendingTransition(R.anim.bot_to_top, R.anim.nothing);
+						// finish();
+						// overridePendingTransition(R.anim.bot_to_top,
+						// R.anim.nothing);
 					} else {
 						Builder builder = new Builder(MainSplashActivity.this);
 						builder.setCancelable(false);
@@ -130,9 +148,12 @@ public class MainSplashActivity extends BaseActivtiy {
 									public void onClick(DialogInterface dialog,
 											int which) {
 										finish();
-										overridePendingTransition(R.anim.bot_to_top, R.anim.nothing);
+										// overridePendingTransition(
+										// R.anim.bot_to_top,
+										// R.anim.nothing);
 									}
 								});
+						builder.show();
 					}
 				}
 
