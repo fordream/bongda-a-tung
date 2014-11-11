e5975504fbd74b2016d1b2739672beca307e35ba
Vương Văn Trường <truongvv@atmarkcafe.org>
2014-11-11 10:19:24 +0700

    add country adapter

--- BongDa/src/com/app/bongda/fragment/CountryFragment.java
+++ BongDa/src/com/app/bongda/fragment/CountryFragment.java
@@ -24,6 +24,7 @@ import com.app.bongda.service.BongDaServiceManager;
 import com.app.bongda.util.ByUtils;
 import com.app.bongda.util.CommonAndroid;
 import com.app.bongda.view.HeaderView;
+import com.app.bongda.view.adapter.cursor.CountryCusorAdapter;
 import com.vnp.core.datastore.database.CountryTable;
 
 public class CountryFragment extends BaseFragment {
@@ -35,29 +36,32 @@ public class CountryFragment extends BaseFragment {
 		this.onItemClickListener = onItemClickListener;
 	}
 
-	int count = 0;
-	private CountryAdapter countryAdapter = new CountryAdapter();
-
-	private class CountryAdapter extends BongDaBaseAdapter {
-
-		@Override
-		public int getLayout() {
-			return R.layout.country_item;
-		}
-
-		@Override
-		public void showData(Object item, View convertView) {
-			TextView textView = (TextView) convertView.findViewById(R.id.textView1);
-			textView.setText(((Country) item).getName());
-			String image1 = ((Country) item).logoCountry();
-			ImageLoaderUtils.getInstance(getActivity()).DisplayImage(image1, (ImageView) convertView.findViewById(R.id.imageView1));
-		}
-	}
+	// int count = 0;
+	// private CountryAdapter countryAdapter = new CountryAdapter();
+	//
+	// private class CountryAdapter extends BongDaBaseAdapter {
+	//
+	// @Override
+	// public int getLayout() {
+	// return R.layout.country_item;
+	// }
+	//
+	// @Override
+	// public void showData(Object item, View convertView) {
+	// TextView textView = (TextView) convertView.findViewById(R.id.textView1);
+	// textView.setText(((Country) item).getName());
+	// String image1 = ((Country) item).logoCountry();
+	// ImageLoaderUtils.getInstance(getActivity()).DisplayImage(image1,
+	// (ImageView) convertView.findViewById(R.id.imageView1));
+	// }
+	// }
 
 	@Override
 	public int getLayout() {
 		return R.layout.country;
 	}
+
+	CountryCusorAdapter countryCusorAdapter;
 
 	@Override
 	public void onInitCreateView(View view) {
@@ -70,71 +74,80 @@ public class CountryFragment extends BaseFragment {
 		ListView listView = (ListView) view.findViewById(R.id.listView1);
 		listView.setOnItemClickListener(onItemClickListener);
 
-		listView.setAdapter(countryAdapter);
+		// listView.setAdapter(countryAdapter);
 
 		if (ByUtils.USEGROUPVIEW) {
 			Cursor cursor = BongDaServiceManager.getInstance().getBongDaService().query(new CountryTable().getTableName(), null);
-			listView.setAdapter(new CursorAdapter(view.getContext(), cursor) {
-
-				@Override
-				public View newView(Context arg0, Cursor arg1, ViewGroup arg2) {
-					View view = (View) ((LayoutInflater) arg2.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.country_item, null);
-					showData(arg1, view);
-					return view;
-				}
-
-				public void showData(Cursor cursor, View convertView) {
-					TextView textView = (TextView) convertView.findViewById(R.id.textView1);
-					textView.setText(cursor.getString(cursor.getColumnIndex("sTenQuocGia")));
-					String image1 = cursor.getString(cursor.getColumnIndex("sLogo"));
-					ImageLoaderUtils.getInstance(getActivity()).DisplayImage(image1, (ImageView) convertView.findViewById(R.id.imageView1));
-				}
-
-				@Override
-				public void bindView(View arg0, Context arg1, Cursor arg2) {
-					showData(arg2, arg0);
-				}
-			});
-			// loadata();
+			countryCusorAdapter = new CountryCusorAdapter(view.getContext(), cursor, true);
+			listView.setAdapter(countryCusorAdapter);
+			// listView.setAdapter(new CursorAdapter(view.getContext(), cursor)
+			// {
+			// @Override
+			// public View newView(Context arg0, Cursor arg1, ViewGroup arg2) {
+			// View view = (View) ((LayoutInflater)
+			// arg2.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.country_item,
+			// null);
+			// showData(arg1, view);
+			// return view;
+			// }
+			//
+			// public void showData(Cursor cursor, View convertView) {
+			// TextView textView = (TextView)
+			// convertView.findViewById(R.id.textView1);
+			// textView.setText(cursor.getString(cursor.getColumnIndex("sTenQuocGia")));
+			// String image1 = cursor.getString(cursor.getColumnIndex("sLogo"));
+			// ImageLoaderUtils.getInstance(getActivity()).DisplayImage(image1,
+			// (ImageView) convertView.findViewById(R.id.imageView1));
+			// }
+			//
+			// @Override
+			// public void bindView(View arg0, Context arg1, Cursor arg2) {
+			// showData(arg2, arg0);
+			// }
+			// });
 		}
 	}
 
-	private void loadata() {
-		if (callbackAPI == null)
-			callbackAPI = new ICallbackAPI() {
-				@Override
-				public void onSuccess(String response) {
-					String string_temp = CommonAndroid.parseXMLAction(response);
-					if (!string_temp.equalsIgnoreCase("")) {
-						try {
-							JSONArray jsonarray = new JSONArray(string_temp);
-							for (int i = 0; i < jsonarray.length(); i++) {
-								countryAdapter.addItem(new Country(jsonarray.getJSONObject(i).getString("iID_MaQuocGia"), jsonarray.getJSONObject(i).getString("sTenQuocGia"), jsonarray.getJSONObject(
-										i).getString("sLogo")));
-							}
-							countryAdapter.notifyDataSetChanged();
-						} catch (JSONException e) {
-						}
-					}
-				}
-
-				@Override
-				public void onError(String message) {
-				}
-			};
-		BongDaServiceManager.getInstance().getBongDaService().callApi(getCurrentTime(), callbackAPI, ByUtils.wsFootBall_Quocgia);
-	}
-
-	@Override
-	public void onResume() {
-		super.onResume();
-		loadata();
-	}
-
-	@Override
-	public void onPause() {
-		super.onPause();
-	}
+	// private void loadata() {
+	// if (callbackAPI == null)
+	// callbackAPI = new ICallbackAPI() {
+	// @Override
+	// public void onSuccess(String response) {
+	// String string_temp = CommonAndroid.parseXMLAction(response);
+	// if (!string_temp.equalsIgnoreCase("")) {
+	// try {
+	// JSONArray jsonarray = new JSONArray(string_temp);
+	// for (int i = 0; i < jsonarray.length(); i++) {
+	// countryAdapter.addItem(new
+	// Country(jsonarray.getJSONObject(i).getString("iID_MaQuocGia"),
+	// jsonarray.getJSONObject(i).getString("sTenQuocGia"),
+	// jsonarray.getJSONObject(
+	// i).getString("sLogo")));
+	// }
+	// countryAdapter.notifyDataSetChanged();
+	// } catch (JSONException e) {
+	// }
+	// }
+	// }
+	//
+	// @Override
+	// public void onError(String message) {
+	// }
+	// };
+	// BongDaServiceManager.getInstance().getBongDaService().callApi(getCurrentTime(),
+	// callbackAPI, ByUtils.wsFootBall_Quocgia);
+	// }
+	//
+	// @Override
+	// public void onResume() {
+	// super.onResume();
+	// loadata();
+	// }
+	//
+	// @Override
+	// public void onPause() {
+	// super.onPause();
+	// }
 
 	@Override
 	public void onInitData() {
